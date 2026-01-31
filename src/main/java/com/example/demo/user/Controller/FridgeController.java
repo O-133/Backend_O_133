f06c1af778d90e991172de0f74fe017ffa0d89f2
seungjun lee <lesj9217@gmail.com>
2026-02-01 08:25:21 +0900

    feat: 영수증 분석 및 재료 추가 api 추가

--- src/main/java/com/example/demo/user/Controller/FridgeController.java
+++ src/main/java/com/example/demo/user/Controller/FridgeController.java
@@ -1,10 +1,13 @@
 package com.example.demo.user.Controller;
 
+import com.example.demo.user.Service.ClovaOcrService;
 import com.example.demo.global.api.ApiResponse;
 import com.example.demo.user.Service.FridgeService;
 import com.example.demo.user.dto.FridgeDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -14,6 +17,7 @@ import java.util.List;
 public class FridgeController {
 
     private final FridgeService fridgeService;
+    private final ClovaOcrService clovaOcrService;
 
     // 대량 추가
     @PostMapping("/{userId}")
@@ -37,10 +41,15 @@ public class FridgeController {
         fridgeService.deleteIngredients(userId, request.getIngredientNames());
         return ApiResponse.ok("선택한 재료들이 삭제되었습니다.");
     }
-    @PostMapping("/{userId}/ocr")
-    public ApiResponse<String> processOcr(@PathVariable Integer userId,
-                                          @RequestBody List<String> ocrTexts) {
-        fridgeService.addIngredientsByOcr(userId, ocrTexts);
-        return ApiResponse.ok("영수증 분석 결과가 냉장고에 반영되었습니다.");
+    // 영수증 OCR 처리 및 재료 추가
+    @PostMapping(value = "/{userId}/ocr", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponse<List<FridgeDto.Info>> processOcr(
+            @PathVariable Integer userId,
+            @RequestPart("file") MultipartFile file
+    ) {
+        List<FridgeDto.Info> result = clovaOcrService.processReceipt(userId, file);
+
+        // 2. 최종적으로 저장된 재료 리스트 반환
+        return ApiResponse.ok(result);
     }
 }
