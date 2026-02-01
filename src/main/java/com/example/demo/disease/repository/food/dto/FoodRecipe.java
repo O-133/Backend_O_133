5aabbd825b6a9d1cb994913f90db23d2629a1c29
minjoon1015 <3283alswns@naver.com>
2026-02-01 10:15:33 +0900

    a

---
+++ src/main/java/com/example/demo/disease/repository/food/dto/FoodRecipe.java
@@ -0,0 +1,12 @@
+package com.example.demo.disease.repository.food.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class FoodRecipe {
+    private String recipe;
+}
